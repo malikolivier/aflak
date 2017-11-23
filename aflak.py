f0fcebab1fb6a27dd23ba13378510c550474ca50
Malik Olivier Boussejra <malik@boussejra.com>
2017-11-23 09:50:20 +0900

    Can visualize FLUX in FITS file
    Initial commit

--- aflak.py
+++ aflak.py
@@ -1,10 +1,29 @@
 import argparse
-import fitsio
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+
+from fits import FITS
+
 
 parser = argparse.ArgumentParser(description='Provide FITS file as input')
 parser.add_argument('fits', metavar='fits-file', help='FITS file to open')
 
 args = parser.parse_args()
 
-fits = fitsio.FITS(args.fits)
-print(fits)
+fits = FITS(args.fits)
+
+app = QtGui.QApplication([])
+
+## Create window with ImageView widget
+win = QtGui.QMainWindow()
+imv = pg.ImageView()
+win.setCentralWidget(imv)
+win.show()
+WINDOWS_TITLE = args.fits
+win.setWindowTitle(WINDOWS_TITLE)
+
+flux = fits.flux()
+imv.setImage(flux)
+
+QtGui.QApplication.instance().exec_()
---
+++ fits.py
@@ -0,0 +1,13 @@
+from astropy.io import fits
+
+class FITS:
+    def __init__(self, name, **kwargs):
+        self.hdulist = fits.open(name, **kwargs)
+        print('Opened file: {}'.format(name))
+        print(self.hdulist.info())
+
+    def wave(self):
+        return self.hdulist['WAVE'].data
+
+    def flux(self):
+        return self.hdulist['FLUX'].data
