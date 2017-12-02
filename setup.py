dfb951598d93b18cef4dea6331020ad1747b6e4d
Malik Olivier Boussejra <malik@boussejra.com>
2017-12-03 02:44:46 +0900

    setup.py: Create setup.py to install aflak
    Install with:
    
        python setup.py install
    
    Create __init__.py to avoid the warning on include the aflak package.
    For the time being, aflak is not used as a package yet, though it most
    probable will in the future (for unit testing, etc.).

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+setup(name='aflak',
+      version='0.0.1',
+      description='Advanced Framework for Learning Astrophysical Knowledge',
+      author='Malik Olivier Boussejra',
+      author_email='malik@boussejra.com',
+      url='https://github.com/malikolivier/aflak',
+      license='GPLv3',
+      packages=['aflak'],
+      install_requires=[
+          'astropy',
+          'pyqtgraph',
+          'PyQt5',
+      ],
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Environment :: Console',
+          'Environment :: X11 Applications :: Qt',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Multimedia :: Graphics',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Topic :: Scientific/Engineering :: Visualization'
+      ])
