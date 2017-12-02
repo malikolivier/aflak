80911f407bb3fd55039375e3b872c2849a8c34c7
Malik Olivier Boussejra <malik@boussejra.com>
2017-12-03 02:03:38 +0900

    window: Can open FITS files interactively
    Can now open FITS files through the File -> Open dialog in the GUI.

--- aflak/window.py
+++ aflak/window.py
@@ -12,8 +12,13 @@ class MainWindow:
         exitAction.setShortcut('Ctrl+Q')
         exitAction.triggered.connect(QtWidgets.qApp.quit)
 
+        openAction = QtGui.QAction('&Open', self.win)
+        openAction.setShortcut('Ctrl+O')
+        openAction.triggered.connect(self._open_file)
+
         menubar = self.win.menuBar()
         fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openAction)
         fileMenu.addAction(exitAction)
 
         self.viewer = viewer.AstroImageViewer()
@@ -23,3 +28,11 @@ class MainWindow:
     def set_fits_file(self, file_path):
         self.win.setWindowTitle(file_path)
         self.viewer.set_fits_file(file_path)
+
+    def _open_file(self):
+        name, _file_type = QtGui.QFileDialog.getOpenFileName(self.win,
+                                                             'Open File')
+        # `name' is an empty string if the user pressed `Cancel' in the
+        # open-file dialog
+        if name != '':
+            self.set_fits_file(name)
