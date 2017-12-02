c3e0e2cc462e546a79c860960ad88ba43f92aff1
Malik Olivier Boussejra <malik@boussejra.com>
2017-12-03 02:12:06 +0900

    aflak: Allow not to provide FITS file on CLI
    If FITS file is not provided in the command line, the GUI will just open
    with no image loaded. You can then load an image using the GUI.
    
    To open aflak with a pre-loaded image, just do:
    
        python aflak

--- aflak/__main__.py
+++ aflak/__main__.py
@@ -6,12 +6,14 @@ import window
 
 
 parser = argparse.ArgumentParser(description='Provide FITS file as input')
-parser.add_argument('fits', metavar='fits-file', help='FITS file to open')
+parser.add_argument('fits', metavar='fits-file', nargs='?',
+                    help='FITS file to open')
 
 args = parser.parse_args()
 
 app = QtGui.QApplication([])
 aflak = window.MainWindow()
-aflak.set_fits_file(args.fits)
+if args.fits is not None:
+    aflak.set_fits_file(args.fits)
 
 QtGui.QApplication.instance().exec_()
