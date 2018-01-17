b1d9a0500691c08d509b9b5704919ac86683fe00
Malik Olivier Boussejra <malik@boussejra.com>
2018-01-17 18:45:40 +0900

    AstroHistogramLUTItem.py: Display an axis below histogram
    Values are wrong  (-2, 0...)

--- aflak/AstroHistogramLUTItem.py
+++ aflak/AstroHistogramLUTItem.py
@@ -1,4 +1,5 @@
 from pyqtgraph.graphicsItems.HistogramLUTItem import HistogramLUTItem
+from pyqtgraph.graphicsItems.AxisItem import AxisItem
 
 
 class AstroHistogramLUTItem(HistogramLUTItem):
@@ -9,3 +10,6 @@ class AstroHistogramLUTItem(HistogramLUTItem):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.plot.setLogMode(False, True)
+        self.yAxis = AxisItem('bottom', linkView=self.vb, parent=self)
+        self.yAxis.setLabel(text="Flux", unit="UNIT")
+        self.layout.addItem(self.yAxis, 3, 1)
