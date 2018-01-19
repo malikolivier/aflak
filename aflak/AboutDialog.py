0779ee841beff2fbfce06799220440583b46e8ee
Malik Olivier Boussejra <malik@boussejra.com>
2018-01-19 18:04:15 +0900

    AboutDialog.py: Set version

--- aflak/AboutDialog.py
+++ aflak/AboutDialog.py
@@ -1,5 +1,6 @@
 from pyqtgraph.Qt import QtWidgets
 
+from . import __version__
 from .AboutDialog_ui import Ui_AboutDialog
 
 
@@ -9,3 +10,4 @@ class AboutDialog(QtWidgets.QDialog):
             self.ui = Ui_AboutDialog()
             self.ui.setupUi(self)
             self.ui.closeButton.clicked.connect(self.close)
+            self.ui.versionLabel.setText(__version__)
