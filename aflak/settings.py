c070d1e9e909a1b7dd3a935cf361efe7206e99ee
Malik Olivier Boussejra <malik@boussejra.com>
2018-01-22 18:40:22 +0900

    settings.py: Make class to manage QSettings

---
+++ aflak/settings.py
@@ -0,0 +1,42 @@
+import datetime
+import sys
+
+from pyqtgraph.Qt import QtCore
+
+
+QtCore.QCoreApplication.setOrganizationName('aflak')
+QtCore.QCoreApplication.setOrganizationDomain('aflak.jp')
+QtCore.QCoreApplication.setApplicationName('aflak')
+
+
+class Settings:
+    settings = QtCore.QSettings()
+
+    @classmethod
+    def addRecentFile(cls, filePath):
+        files = cls.settings.value('RecentFiles')
+        if type(files) is not list:
+            files = []
+        files = [f for f in files if f['path'] != filePath]
+        files.append({
+            'time': datetime.datetime.now(),
+            'path': filePath
+        })
+        cls.settings.setValue('RecentFiles', files)
+
+    @classmethod
+    def getRecentFiles(cls):
+        files = cls.settings.value('RecentFiles')
+        if type(files) is not list:
+            return []
+        else:
+            try:
+                files = sorted(files, key=lambda f: f['time'], reverse=True)
+                return [f['path'] for f in files]
+            except TypeError as e:
+                # In case memory on disk got corrupted, reset memory
+                print('Unexpected error on parsing RecentFiles',
+                      file=sys.stderr)
+                print(e, file=sys.stderr)
+                cls.settings.setValue('RecentFiles', [])
+                return []
